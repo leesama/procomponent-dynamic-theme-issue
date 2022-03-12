dc6d41cfc537db165818d57d6a88047256ccf063
tutu <1515112287@qq.com>
2022-03-12 15:18:54 +0800

    issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import { Button, ConfigProvider } from "antd";
+import "antd/dist/antd.variable.min.css";
+const LazyProTable = lazy(() => import("./LazyProTable"));
+const LazyButton = lazy(() => import("./LazyButton"));
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Button
+      type="primary"
+      onClick={() =>
+        ConfigProvider.config({
+          theme: {
+            primaryColor: "#25b864",
+          },
+        })
+      }
+    >
+      change color
+    </Button>
+    <Suspense fallback={"loading"}>
+      {/* <LazyProTable /> */}
+      <LazyButton />
+    </Suspense>
+  </div>
+);
 
 export default App;
---
+++ src/LazyButton.js
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+import React from "react";
+
+export default function LazyButton() {
+  return (
+    <Button type="primary" key="primary">
+      创建应用
+    </Button>
+  );
+}
