baf2caa8da3abb4c69f93a9ae688c79c06982fae
tutu <1515112287@qq.com>
2022-03-12 15:21:36 +0800

    issue

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => (
       change color
     </Button>
     <Suspense fallback={"loading"}>
-      {/* <LazyProTable /> */}
+      <LazyProTable />
       <LazyButton />
     </Suspense>
   </div>
--- src/LazyButton.js
+++ src/LazyButton.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function LazyButton() {
   return (
     <Button type="primary" key="primary">
-      创建应用
+      lazyButton
     </Button>
   );
 }
